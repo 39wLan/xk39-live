414892103235f4b4be60491ab149bcd2c4965dd8
39 <2515193410@qq.com>
2021-07-20 18:30:11 +0800

    21.07.20 07.20|608

--- leetcode/src/main/java/xk/Solution.java
+++ leetcode/src/main/java/xk/Solution.java
@@ -1,15 +1,181 @@
 package xk;
 
-import org.junit.jupiter.api.Test;
+
+
+import org.junit.Test;
 
 import java.util.*;
 
 class Solution {
+
     @Test
-    public void test(){
+    public void myTest(){
         String s1="1234123412312341234";
         System.out.println(Arrays.toString(getNext(s1)));
     }
+    
+    
+    public int lengthOfLIS(int[] nums) {
+        int len = 1, n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] d = new int[n + 1];
+        d[len] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                /**
+                 * 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+                 */
+                int l = 1, r = len, pos = 0;
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
+    
+    
+    public int findPeakElement(int[] nums) {
+        if(nums.length==1){
+            return 0;
+        }
+        int left=0;
+        int right= nums.length-1;
+        while (left<=right){
+            int mid=(left+right)/2;
+            int trend=helpFindPeakElement(nums,mid);
+            switch (trend){
+                case 121:
+                    return mid;
+                case 123:
+                    left=mid+1;
+                    break;
+                case 321:
+                case 212:
+                    right=mid-1;
+                    break;
+                default:
+            }
+        }
+        return 0;
+    }
+    
+    public int helpFindPeakElement(int[] nums,int n){
+        if(n==0){
+            if(nums[0]<nums[1]){
+                return 123;
+            }else if(nums[0]>nums[1]){
+                return 121;
+            }
+        }
+        if(n== nums.length-1){
+            if(nums[n]<nums[n-1]){
+                return 321;
+            }else if(nums[n]>nums[n-1]){
+                return 121;
+            }
+        }
+        if(nums[n]>nums[n-1]&&nums[n]>nums[n+1]){
+            return 121;
+        }else if(nums[n]>nums[n-1]&&nums[n]<nums[n+1]){
+            return 123;
+        }else if(nums[n]<nums[n-1]&&nums[n]>nums[n+1]){
+            return 321;
+        }else {
+            return 212;
+        }
+    }
+    
+    
+    
+    
+    
+    
+    public int[] searchRange(int[] nums, int target) {
+        
+        int[] ans=new int[]{-1,-1};
+        if(nums.length==0){
+            return ans;
+        }
+        int right= nums.length-1;
+        int left=0;
+        while(left<right){
+            int mid=(right+left)/2;
+            if(nums[mid]==target){
+                int leftRange=mid-1;
+                int rightRange=mid+1;
+                ans[0]=mid;
+                ans[1]=mid;
+                while(left<=leftRange){
+                    mid=(left+leftRange)/2;
+                    if(nums[mid]==target){
+                        ans[0]=mid;
+                        leftRange=mid-1;
+                    }else {
+                        left=mid+1;
+                    }
+                }
+                while(right>=rightRange){
+                    mid=(right+rightRange)/2;
+                    if(nums[mid]==target){
+                        ans[1]=mid;
+                        rightRange=mid+1;
+                    }else {
+                        right=mid-1;
+                    }
+                }
+                break;
+            }else if(nums[mid]<target){
+                left=mid+1;
+            }else if(nums[mid]>target){
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+    
+    public int search(int[] nums, int target) {
+        if(nums.length==0){
+            return -1;
+        }
+        int right= nums.length-1;
+        return helpSearch(nums,target,0,right);
+    }
+    
+    public int helpSearch(int[] nums,int target,int left,int right){
+        if(left>right){
+            return -1;
+        }
+        if(target==nums[left]){
+            return left;
+        }
+        if(target==nums[right]){
+            return right;
+        }
+        if(left==right){
+            return -1;
+        }
+        if(nums[left]<nums[right]&&(target<nums[left]||target>nums[right])){
+            return -1;
+        }
+        if(nums[left]>nums[right]&&(target<nums[left]&&target>nums[right])){
+            return -1;
+        }
+        int mid=(left+right)/2;
+        return Math.max(helpSearch(nums,target,left,mid),helpSearch(nums,target,mid+1,right));
+    }
+    
     
     public int[] getNext(String s){
         char[] chars=s.toCharArray();
